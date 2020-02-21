e06ae67933eb3e0cde938ea07e54dc375ae3b9af
Ryan Morrisroe <6473229+RyanMorrisroe@users.noreply.github.com>
2020-02-21 01:28:36 -0600

    Initial code

---
+++ HelloWorldForAzureBatch/BatchController/PoolSettings.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace BatchController
+{
+    public class PoolSettings
+    {
+        public string PoolId { get; set; }
+        public string JobId { get; set; }
+        public int PoolNodeCount { get; set; }
+        public string PoolOSFamily { get; set; }
+        public string PoolVMSize { get; set; }
+        public bool ShouldDeleteJob { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            AddSetting(sb, nameof(PoolId), PoolId);
+            AddSetting(sb, nameof(JobId), JobId);
+            AddSetting(sb, nameof(PoolNodeCount), PoolNodeCount);
+            AddSetting(sb, nameof(PoolOSFamily), PoolOSFamily);
+            AddSetting(sb, nameof(PoolVMSize), PoolVMSize);
+            AddSetting(sb, nameof(ShouldDeleteJob), ShouldDeleteJob);
+
+            return sb.ToString();
+        }
+
+        private static void AddSetting(StringBuilder sb, string settingName, object settingValue)
+        {
+            sb.AppendFormat("{0} = {1}", settingName, settingValue).AppendLine();
+        }
+    }
+}
---
+++ HelloWorldForAzureBatch/BatchProgram/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace HelloWorldForAzureBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = args[0];
+            string outputFileDirectory = args[1];
+
+            if(!outputFileDirectory.EndsWith('\\'))
+            {
+                outputFileDirectory += "\\";
+            }
+
+            string inputFileName = ReadFile(inputFilePath);
+            WriteOutput(inputFileName, outputFileDirectory);
+        }
+
+        private static string ReadFile(string filePath)
+        {
+            if(!File.Exists(filePath))
+            {
+                filePath = Directory.GetCurrentDirectory() + filePath;
+            }
+            File.ReadAllText(filePath);
+            return Path.GetFileName(filePath);
+        }
+
+        private static void WriteOutput(string fileName, string outputFileDirectory)
+        {
+            if(!outputFileDirectory.Contains(Directory.GetCurrentDirectory()))
+            {
+                outputFileDirectory = Directory.GetCurrentDirectory() + outputFileDirectory;
+                if(!Directory.Exists(outputFileDirectory))
+                {
+                    Directory.CreateDirectory(outputFileDirectory);
+                }
+            }
+            File.WriteAllText($"{outputFileDirectory}{fileName}", $"Batch program successfully read {fileName}. Hurray!");
+        }
+    }
+}
